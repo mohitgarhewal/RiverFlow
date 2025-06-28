bf4af397d2fce54e50018e4c66ad161d6ce451f2
mohitgarhewal <mohitgarhewal68@gmail.com>
2025-06-29 00:07:23 +0530

    login and SignUp page

---
+++ src/Auth/layout.tsx
@@ -0,0 +1,30 @@
+import { useAuthStore } from "@/store/Auth"
+import { useRouter } from "next/router";
+import { ReactNode } from "react"
+import React from "react";
+
+const Layout =({children} : {children : ReactNode}) => {
+   const {session} = useAuthStore();
+   const router = useRouter()
+    
+   React.useEffect(() =>{
+     if(session){
+        router.push("/")
+     }
+   }, [session , router])
+
+   if(session){
+     return null
+   }
+
+   return(
+    <div>
+        <div>
+            {children}
+        </div>
+    </div>
+   )
+   
+} 
+
+export default Layout
